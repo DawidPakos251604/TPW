f1fe66e13709ae546394beb6938cfc0ce9aae4fd
251604 Pakos Dawid <251604@edu.p.lodz.pl>
2025-03-18 18:14:16 +0100

    Data and Logic layers implementation

---
+++ Logic/LogicAbstractAPI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public abstract class LogicAbstractAPI : IDisposable
+    {
+        #region Layer Factory
+
+        public static LogicAbstractAPI GetLogicLayer()
+        {
+            return modelInstance.Value;
+        }
+
+        #endregion Layer Factory
+
+        #region Layer API
+
+        public static readonly Dimensions GetDimensions = new(10.0, 10.0, 10.0);
+
+        public abstract void Start(int numberOfBalls, Action<IPosition, IBall> upperLayerHandler);
+
+        #region IDisposable
+
+        public abstract void Dispose();
+
+        #endregion IDisposable
+
+        #endregion Layer API
+
+        #region private
+
+        private static Lazy<LogicAbstractAPI> modelInstance = new Lazy<LogicAbstractAPI>(() => new LogicImplementation());
+
+        #endregion private
+    }
+    /// <summary>
+    /// Immutable type representing table dimensions
+    /// </summary>
+    /// <param name="BallDimension"></param>
+    /// <param name="TableHeight"></param>
+    /// <param name="TableWidth"></param>
+    /// <remarks>
+    /// Must be abstract
+    /// </remarks>
+    public record Dimensions(double BallDimension, double TableHeight, double TableWidth);
+
+    public interface IPosition
+    {
+        double x { get; init; }
+        double y { get; init; }
+    }
+
+    public interface IBall
+    {
+        event EventHandler<IPosition> NewPositionNotification;
+    }
+}
---
+++ Logic/LogicImplementation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderneathLayerAPI = Data.DataAbstractAPI;
+
+namespace Logic
+{
+    internal class LogicImplementation : LogicAbstractAPI
+    {
+        #region ctor
+
+        public LogicImplementation() : this(null)
+        { }
+
+        internal LogicImplementation(UnderneathLayerAPI? underneathLayer)
+        {
+            layerBellow = underneathLayer == null ? UnderneathLayerAPI.GetDataLayer() : underneathLayer;
+        }
+
+        #endregion ctor
+
+        #region LogicAbstractAPI
+
+        public override void Dispose()
+        {
+            if (Disposed)
+                throw new ObjectDisposedException(nameof(LogicImplementation));
+            layerBellow.Dispose();
+            Disposed = true;
+        }
+
+        public override void Start(int numberOfBalls, Action<IPosition, IBall> upperLayerHandler)
+        {
+            if (Disposed)
+                throw new ObjectDisposedException(nameof(LogicImplementation));
+            if (upperLayerHandler == null)
+                throw new ArgumentNullException(nameof(upperLayerHandler));
+            layerBellow.Start(numberOfBalls, (startingPosition, databall) => upperLayerHandler(new Position(startingPosition.x, startingPosition.x), new Ball(databall)));
+        }
+
+        #endregion BusinessLogicAbstractAPI
+
+        #region private
+
+        private bool Disposed = false;
+
+        private readonly UnderneathLayerAPI layerBellow;
+
+        #endregion private
+
+        #region TestingInfrastructure
+
+        [Conditional("DEBUG")]
+        internal void CheckObjectDisposed(Action<bool> returnInstanceDisposed)
+        {
+            returnInstanceDisposed(Disposed);
+        }
+
+        #endregion TestingInfrastructure
+    }
+}
