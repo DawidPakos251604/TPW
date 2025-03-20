ebc1f269ab23777c38a94364b81864b112551a72
251604 Pakos Dawid <251604@edu.p.lodz.pl>
2025-03-20 21:23:00 +0100

    PresentationModel implementation

--- PresentationModel/ModelAbstractAPI.cs
+++ PresentationModel/ModelAbstractAPI.cs
@@ -1,12 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace Presentation.Model
 {
-    class ModelAbstractAPI
+    public interface IBall : INotifyPropertyChanged
     {
+        double Top { get; }
+        double Left { get; }
+        double Diameter { get; }
+    }
+
+    public abstract class ModelAbstractApi : IObservable<IBall>, IDisposable
+    {
+        public static ModelAbstractApi CreateModel()
+        {
+            return modelInstance.Value;
+        }
+
+        public abstract void Start(int numberOfBalls);
+
+        #region IObservable
+
+        public abstract IDisposable Subscribe(IObserver<IBall> observer);
+
+        #endregion IObservable
+
+        #region IDisposable
+
+        public abstract void Dispose();
+
+        #endregion IDisposable
+
+        #region private
+
+        private static Lazy<ModelAbstractApi> modelInstance = new Lazy<ModelAbstractApi>(() => new ModelImplementation());
+
+        #endregion private
     }
 }
