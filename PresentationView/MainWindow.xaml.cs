c69f7f5840889211180acfb2df1e31651a8c4bc6
251604 Pakos Dawid <251604@edu.p.lodz.pl>
2025-03-23 18:00:44 +0100

    Balls are visible in MainWindow

--- PresentationView/MainWindow.xaml.cs
+++ PresentationView/MainWindow.xaml.cs
@@ -9,16 +9,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Presentation.ViewModel;
 
-namespace PresentationView;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace PresentationView
 {
-    public MainWindow()
+    /// <summary>
+    /// View implementation
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            Random random = new Random();
+            InitializeComponent();
+            MainWindowViewModel viewModel = (MainWindowViewModel)DataContext;
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+            viewModel.Start(random.Next(5, 10));
+        }
+
+        /// <summary>
+        /// Raises the <seealso cref="System.Windows.Window.Closed"/> event.
+        /// </summary>
+        /// <param name="e">An <see cref="EventArgs"/> that contains the event data.</param>
+        protected override void OnClosed(EventArgs e)
+        {
+            if (DataContext is MainWindowViewModel viewModel)
+                viewModel.Dispose();
+            base.OnClosed(e);
+        }
     }
 }
--- PresentationViewModel/MainWindowViewModel.cs
+++ PresentationViewModel/MainWindowViewModel.cs
@@ -1,7 +1,74 @@
-﻿namespace Presentation.ViewModel
-{
-    public class MainWinowViewModel
-    {
+﻿using Presentation.Model;
+using System.Collections.ObjectModel;
 
+using ModelIBall = Presentation.Model.IBall;
+
+namespace Presentation.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase, IDisposable
+    {
+        #region ctor
+
+        public MainWindowViewModel() : this(null)
+        { }
+
+        internal MainWindowViewModel(ModelAbstractApi modelLayerAPI)
+        {
+            ModelLayer = modelLayerAPI == null ? ModelAbstractApi.CreateModel() : modelLayerAPI;
+            Observer = ModelLayer.Subscribe<ModelIBall>(x => Balls.Add(x));
+        }
+
+        #endregion ctor
+
+        #region public API
+
+        public void Start(int numberOfBalls)
+        {
+            if (Disposed)
+                throw new ObjectDisposedException(nameof(MainWindowViewModel));
+            ModelLayer.Start(numberOfBalls);
+            Observer.Dispose();
+        }
+
+        public ObservableCollection<ModelIBall> Balls { get; } = new ObservableCollection<ModelIBall>();
+
+        #endregion public API
+
+        #region IDisposable
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!Disposed)
+            {
+                if (disposing)
+                {
+                    Balls.Clear();
+                    Observer.Dispose();
+                    ModelLayer.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                Disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (Disposed)
+                throw new ObjectDisposedException(nameof(MainWindowViewModel));
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion IDisposable
+
+        #region private
+
+        private IDisposable Observer = null;
+        private ModelAbstractApi ModelLayer;
+        private bool Disposed = false;
+
+        #endregion private
     }
 }
