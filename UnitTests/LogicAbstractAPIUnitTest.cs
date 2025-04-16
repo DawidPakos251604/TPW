c55e981ec9106607cce5888144aa8e4242582e87
251604 Pakos Dawid <251604@edu.p.lodz.pl>
2025-04-16 15:23:14 +0200

    LogicAbstractAPIUnitTest

---
+++ UnitTests/LogicAbstractAPIUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using Logic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class LogicAbstractAPIUnitTest
+    {
+        [TestMethod]
+        public void SingletonTestMethod()
+        {
+            LogicAbstractAPI instance1 = LogicAbstractAPI.GetLogicLayer();
+            LogicAbstractAPI instance2 = LogicAbstractAPI.GetLogicLayer();
+            Assert.AreSame< LogicAbstractAPI>(instance1, instance2);
+        }
+    }
+}
